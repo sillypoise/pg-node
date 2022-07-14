1aa02cf6d0e55d0c42745fa3d261e56524a591f7
sillypoise <somewhatjose@gmail.com>
2022-07-14 12:24:21 -0500

    Added basic environment variable for PORT in base project, making it more flexible for later projects in the repo

--- projects/vanilla-web-server/src/server.ts
+++ projects/vanilla-web-server/src/server.ts
@@ -1,9 +1,13 @@
+import dotenv from "dotenv";
 import { createServer } from "http";
+
+dotenv.config();
+const PORT = process.env["PORT"] || 8000;
 
 let server = createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello World\n");
+    res.end("Hello Chill\n");
 });
 
-console.log("Listening on port 8000");
-server.listen(8000);
+console.log(`Listening on port ${PORT}`);
+server.listen(PORT);
