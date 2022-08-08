ea171d9ec489e96273053f4bb433ff7e7e6413ef
sillypoise <somewhatjose@gmail.com>
2022-08-08 15:30:06 -0500

    Added basic SQLite databse to learn the workings of it

---
+++ projects/sqlite-basic/src/server.ts
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+import { createServer, IncomingMessage, ServerResponse } from "http";
+
+dotenv.config();
+const PORT = process.env["PORT"] || 8000;
+
+let server = createServer((req: IncomingMessage, res: ServerResponse) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello World\n");
+});
+
+console.log(`🚀 Up and running at port: ${PORT}`);
+server.listen(PORT);
