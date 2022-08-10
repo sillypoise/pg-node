9b419a69722b7be7505a451bb6731eca875d6495
sillypoise <somewhatjose@gmail.com>
2022-08-10 17:39:56 -0500

    Created the "/auth" endpoint where the hash is checked agains input passwd. Still need to do some refactoring buy main gist is there working. Next we need to create cookies for sessions.

--- projects/auth-vanilla/src/routes/api.ts
+++ projects/auth-vanilla/src/routes/api.ts
@@ -1,13 +1,37 @@
 import express, { Request, Response } from "express";
-import { hash } from "argon2";
+import { hash, verify } from "argon2";
 import { db } from "../services/db";
 
 let api = express.Router();
 
 // api.use("/auth", authRouter);
-api.get("/auth", (_req: Request, res: Response) => {
+api.post("/auth", async (req: Request, res: Response) => {
     try {
-        return res.status(200).json("Authentication Endpoint");
+        let { email, passwd }: { email: string; passwd: string } = req.body;
+
+        let user: { email: string; passwd: string } | undefined = await (
+            await db
+        ).get("SELECT email, passwd FROM users WHERE email = :email", {
+            ":email": email,
+        });
+
+        if (!user) {
+            throw new Error("User not found");
+        }
+
+        let { passwd: userHash } = user;
+
+        let isValidHash = await verify(userHash, passwd);
+
+        if (isValidHash) {
+            console.log("Success! Here's your token: 🍪");
+        } else {
+            throw new Error(
+                "An error has occurred. Let me obfuscate that for you. Try again."
+            );
+        }
+
+        return res.status(200).json("🔑 Authentication Endpoint");
     } catch (e: any) {
         return res.status(500).json({
             error: e.message,
@@ -15,7 +39,7 @@ api.get("/auth", (_req: Request, res: Response) => {
     }
 });
 
-api.post("/auth/register", async (req: Request, res: Response) => {
+api.post("/register", async (req: Request, res: Response) => {
     try {
         let { email, passwd }: { email: string; passwd: string } = req.body;
         let hashedPasswd = await hash(passwd);
@@ -30,7 +54,7 @@ api.post("/auth/register", async (req: Request, res: Response) => {
 
         return res.status(200).json("📝 Registration Successful");
     } catch (e) {
-        return res.status(500).json("erroroni");
+        return res.status(500).json("An error ocurred during registration");
     }
 });
 
