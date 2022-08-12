80dc02b71a4276b78103d4c7738303eb63c7d13d
sillypoise <somewhatjose@gmail.com>
2022-08-12 17:19:13 -0500

    added express-session package, no store setup yet. Also created the local sqlite db but that isnt tracked

--- projects/server-side-sessions/src/app.ts
+++ projects/server-side-sessions/src/app.ts
@@ -1,11 +1,28 @@
 import dotenv from "dotenv";
 import express from "express";
+import session from "express-session";
 
 import { api } from "./routes/api";
 
 dotenv.config();
 
+let cookieSecret = String(process.env["COOKIE_SECRET"]!);
+
 let app = express();
+
+app.use(
+    session({
+        secret: cookieSecret,
+        cookie: {
+            signed: true,
+            httpOnly: true,
+        },
+        resave: false,
+        saveUninitialized: false,
+        name: "serverZesh",
+        // store
+    })
+);
 
 app.use(express.json());
 
