5d03d781172ac6cf5812a24de05d87295cdbf559
sillypoise <somewhatjose@gmail.com>
2022-08-18 16:07:56 -0500

    changed the file structure to support multiple social sign-ons more easily. Basically applied a more modula folder structre. Pre-optimized? sure. But who cares, i like it

--- projects/auth-oauth-passport/src/routes/auth/auth.router.ts
+++ projects/auth-oauth-passport/src/routes/auth/auth.router.ts
@@ -1,22 +1,8 @@
 import express from "express";
-import passport from "passport";
-import { httpGoogleAuth, httpGoogleAuthCallback } from "./auth.controller";
+import { googleOAuthRouter } from "../google/googleOAuth.router";
 
 let authRouter = express.Router();
 
-// Route for authenticating with Google
-authRouter.get("/google", passport.authenticate("google"), httpGoogleAuth);
-
-// Route for callback after authenticating with Google,
-// must match the callback URL in the Google App settings
-authRouter.get(
-    "/google/callback",
-    passport.authenticate("google", {
-        failureRedirect: "/v1/auth/google/failure",
-        successRedirect: "/v1/home",
-        session: false,
-    }),
-    httpGoogleAuthCallback
-);
+authRouter.use("/google", googleOAuthRouter);
 
 export { authRouter };
