5ac457e2bb499592c130463aa64804732752ea23
sillypoise <somewhatjose@gmail.com>
2022-11-01 16:28:58 -0500

    p/auth-ory-kratos: Created the dumb route regstrator helper function from the original repo

--- projects/auth-ory-kratos/src/app.ts
+++ projects/auth-ory-kratos/src/app.ts
@@ -1,4 +1,5 @@
-import express, { Request, Response } from "express";
+import type { V0alpha2ApiInterface } from "@ory/client";
+import express, { Application, NextFunction, Request, Response } from "express";
 
 let app = express();
 
@@ -8,10 +9,37 @@ app.get("/", (_req: Request, res: Response) => {
     res.status(200).json("Aliiiive");
 });
 
-app.get("*", (_req: Request, res: Response) => {
-    res.status(404).json("404 - page not found");
-});
+// app.get("*", (_req: Request, res: Response) => {
+//     res.status(404).json("404 - page not found");
+// });
+
+function register404Route(app: any, createHelpers?: any) {
+    app.get("*", (_req: Request, res: Response) => {
+        res.status(404).json("404 - Page not found");
+    });
+}
+
+// * Route Registration
+register404Route(app);
 
 // RouteRegistrator
+interface RouteOptions {
+    sdk: V0alpha2ApiInterface;
+    apiBaseUrl: string;
+    kratosBrowserUrl: string;
+    logo?: string;
+}
+
+type RouteOptionsCreator = (req: Request) => RouteOptions;
+
+type RouteCreator = (
+    opts: RouteOptionsCreator
+) => (req: Request, res: Response, next: NextFunction) => void;
+
+type RouteRegistrator = (
+    app: Application,
+    createHelpers?: RouteOptionsCreator,
+    route?: string
+) => void;
 
 export { app };
