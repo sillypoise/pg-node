b1380c1d60d2f03a17a941919b232d6ce262eb45
sillypoise <somewhatjose@gmail.com>
2022-11-01 16:17:53 -0500

    p/auth-ory-kratos: created a catch all 404 route that responds with not found.

--- projects/auth-ory-kratos/src/app.ts
+++ projects/auth-ory-kratos/src/app.ts
@@ -8,4 +8,10 @@ app.get("/", (_req: Request, res: Response) => {
     res.status(200).json("Aliiiive");
 });
 
+app.get("*", (_req: Request, res: Response) => {
+    res.status(404).json("404 - page not found");
+});
+
+// RouteRegistrator
+
 export { app };
