eb9e28d815ad482835d790c7f80133cd7fffa331
sillypoise <somewhatjose@gmail.com>
2022-12-13 21:06:49 -0500

    auth-ory-kratos: tinkering

--- projects/auth-ory-kratos/src/app.ts
+++ projects/auth-ory-kratos/src/app.ts
@@ -14,43 +14,8 @@ app.get("*", (_req: Request, res: Response) => {
     res.status(404).json("404 - page not found");
 });
 
-app.get("/login", (req: Request, res: Response, next: NextFunction) => {
-    // Possible query params
-    let {
-        flow,
-        aal = "",
-        refresh = "",
-        return_to = "",
-        login_challenge,
-    } = req.query;
-
-    // Let's parse the initFlowQuery query params
-    let initFlowQuery = new URLSearchParams({
-        aal: aal.toString(),
-        refresh: refresh.toString(),
-        return_to: return_to.toString(),
-    });
+app.get("/registration", (req, res) => {
+    console.log(res);
 });
-
-// * Route Registration
-// register404Route(app);
-interface RouteOptions {
-    sdk: V0alpha2ApiInterface;
-    apiBaseUrl: string;
-    kratosBrowserUrl: string;
-    logo?: string;
-}
-
-type RouteOptionsCreator = (req: Request) => RouteOptions;
-
-type RouteCreator = (
-    opts: RouteOptionsCreator
-) => (req: Request, res: Response, next: NextFunction) => void;
-
-type RouteRegistrator = (
-    app: Application,
-    createHelpers?: RouteOptionsCreator,
-    route?: string
-) => void;
 
 export { app };
